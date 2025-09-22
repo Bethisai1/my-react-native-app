bf426a8e1409bd5bf9b66ab2cc3c561a7f80ebdc
Bethi sai <119121523+Bethisai1@users.noreply.github.com>
2025-09-22 12:41:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>this is the final one </Text>
+      <Text>this is the admin one </Text>
     </View>
   );
 }
